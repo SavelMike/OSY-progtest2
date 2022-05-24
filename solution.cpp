039a44e22c42bdfca9305b96b4ed36f1b83d7202
Mike <savelievmv@gmail.com>
2022-05-25 00:18:58 +0200

    allocatePage() fix, memMgr() -  allocate CMM object dynamically

--- solution.cpp
+++ solution.cpp
@@ -36,6 +36,7 @@ public:
 	uint32_t nrPages() { return m_PageNum; }
 	void savePageDir(uint32_t pageNum);
 	void freePageDirs(uint32_t pageNum);
+	void printFreeList(const char* text);
 private:
 	uint32_t m_MemFreeListHead;
 	uint32_t m_SwapFreeListHead;
@@ -156,15 +157,15 @@ uint32_t FreeSpaceManager::allocatePage(bool isForPageDir) {
 					continue;
 				}
 				// Found candidate for swapping out
-				pageNum = pte[k].frameNumber;
+				pageNum = pte[l].frameNumber;
 				uint32_t swapPageNum = allocateSwapPage();
-				m_writePage(pte[k].frameNumber, swapPageNum);
-				pte[k].present = 0;
+				m_writePage(pte[l].frameNumber, swapPageNum);
+				pte[l].present = 0;
 				if (swapPageNum >= m_SwapPageNum) {
 					exit(6);
 				}
-				pte[k].frameNumber = swapPageNum;
-				pte[k].swaped = 1;
+				pte[l].frameNumber = swapPageNum;
+				pte[l].swaped = 1;
 				if (isForPageDir) {
 					savePageDir(pageNum);
 				}
@@ -178,6 +179,7 @@ uint32_t FreeSpaceManager::allocatePage(bool isForPageDir) {
 }
 
 void FreeSpaceManager::freePage(uint32_t pageNum) {
+	assert(pageNum < this->m_PageNum);
 	this->m_MemFreeList[pageNum] = this->m_MemFreeListHead;
 	this->m_MemFreeListHead = pageNum;
 	for (unsigned i = 0; i < PROCESS_MAX; i++) {
@@ -188,206 +190,21 @@ void FreeSpaceManager::freePage(uint32_t pageNum) {
 	}
 }
 
-#if 0
-
-
-// Two bit per slot
-// 00 page is free
-// 01 - page is used, not swappable, level1 page table or BitMap page
-// 10 - page is used. not swappable, level2 page table
-// 11 - page swappable
-class CMemoryBitMap {
-private:
-	uint8_t* m_BitMap;
-	uint32_t m_MapSize; // in bits
-	uint8_t* m_SwapBitMap;
-	uint32_t m_SwapSize;
-	bool  (* m_readPage) ( uint32_t memFrame, uint32_t diskPage );
-	bool  (* m_writePage) ( uint32_t memFrame, uint32_t diskPage );
-public:
-	static constexpr uint32_t FOR_PAGE_DIR = 1;
-	static constexpr uint32_t FOR_PAGE_TABLE = 2;
-	static constexpr uint32_t FOR_PAGE = 3;
-	CMemoryBitMap(void* memory, uint32_t mapSize,  uint32_t swapSize,
-		      bool  (* readPage) ( uint32_t memFrame, uint32_t diskPage ),
-		      bool  (*writePage) ( uint32_t memFrame, uint32_t diskPage ))
-		: m_BitMap((uint8_t*)memory), m_MapSize(mapSize), m_SwapSize(swapSize),
-		  m_readPage(readPage), m_writePage(writePage) {
-			// Use page number 0 for map of free used pages
-			for (uint32_t i = 0; i < mapSize / 4; i++)
-				m_BitMap[i] = 0;
-			// Mark 0th page as used, not swappable
-			m_BitMap[0] |= 1;
-			// Initialize swap bitMap
-			m_SwapBitMap = m_BitMap + mapSize / 4;
-			for (uint32_t i = 0; i < swapSize / 8; i++) {
-				m_SwapBitMap[i] = 0;
-			}
+void FreeSpaceManager::printFreeList(const char* text) {
+	uint32_t cur = this->m_MemFreeListHead;
+	while (cur != UINT32_MAX) {
+		cout << cur << ", ";
+		cur = this->m_MemFreeList[cur];
 	}
-
-	bool   readPage ( uint32_t memFrame, uint32_t diskPage ) {
-		return m_readPage(memFrame, diskPage);
+	cout << "\n";
+	cur = this->m_SwapFreeListHead;
+	cout << "head " << cur << "; " << "SwapFreeList:\n";
+	while (cur != UINT32_MAX) {
+		cout << cur << ", ";
+		cur = this->m_SwapFreeList[cur];
 	}
-
-	uint32_t allocateSwapPage(void) {
-		uint8_t mask;
-		for (uint32_t i = 0; i  < m_SwapSize / 8; i++ ) {
-			if (m_SwapBitMap[i] == 255)
-				continue;
-			for (int j  = 0; j < 8; j++) {
-				mask = 1 << j;
-				if (m_SwapBitMap[i] & mask)
-					continue;
-				// Mark the bit as used
-				m_SwapBitMap[i] |= mask;
-				return i * 8 + j;
-			}
-		}
-
-		// No free page
-		return (uint32_t)-1;
-	}
-
-	void freeSwapPage(uint32_t pageNum) {
-		uint8_t mask;
-		uint32_t index = pageNum / 8;
-		int shift = pageNum % 8;
-		mask = 1 << shift;
-		if (m_SwapBitMap[index] & mask) {
-			// Mark the bit as free
-			m_SwapBitMap[index] &= ~mask;
-		} else {
-			cout << "error 1\n";
-			exit(1);
-		}
-	}
-
-	/**
-	 * allocate free page
-	 *
-	 * every page is addressed by 2 bits: 00 - for free page, 01 -
-	 * for level 1 page table, 10 - for level 2 page table, 11 -
-	 * for address space page. If free page is found - it is
-	 * marked as used according to \goal.
-	 * If all pages are used, look for level 2 page tables, swap
-	 * out address space page the page table points to and use the
-	 * swapped page for allocation.
-	 *
-	 * \param[in] goal defines what page is allocated for
-	 *
-	 * \retval number if allocated page
-	 * \retval -1 if failed
-	 */
-	uint32_t allocatePage(int goal) {
-		uint8_t mask;
-		for (uint32_t i = 0; i  < m_MapSize / 4; i++ ) {
-			for (int j = 0; j < 8; j += 2) {
-				mask = 3 << j;
-				if (m_BitMap[i] & mask) {
-					// Page is used: 01, 10 or 11 is found
-					continue;
-				}
-				// Page is free
-				switch (goal) {
-				case CMemoryBitMap::FOR_PAGE_DIR:
-					// allocating for level 1 page table: mark with 01
-					m_BitMap[i] |= (1 << j);
-					break;
-				case CMemoryBitMap::FOR_PAGE_TABLE:
-					// allocating for level 2 page table: mark with 10
-					m_BitMap[i] |= (1 << (j + 1));
-					break;
-				case CMemoryBitMap::FOR_PAGE:
-					// allocating for address space page: mark with 11
-					m_BitMap[i] |= (3 << j);
-					break;
-				}
-				if ((i * 4 + j / 2 ) >= m_MapSize) {
-					exit(4);
-				}
-				return (i * 4 + j / 2);
-			}
-		}
-
-		// No free page
-		// Find level 2 page table
-		for (uint32_t i = 0; i < m_MapSize / 4; i++) {
-			for (int j = 0; j < 8; j  += 2) {
-				uint8_t lev2mask = 2 << j;
-				mask = (3 << j);
-				if ((m_BitMap[i] & mask) == lev2mask) {
-					// Found level 2 page table
-					uint32_t pageNum = i * 4 + j / 2;
-					// m_BitMap == MemStart
-					struct pte* pte  = (struct pte*)(m_BitMap + pageNum * 4096);
-					for (unsigned k = 0; k < CCPU::PAGE_SIZE / sizeof (pte[0]); k++) {
-						if (pte[k].present == 1) {
-							// Found page to be swaped out
-							uint32_t rc = pte[k].frameNumber;
-							uint32_t swapPageNum = allocateSwapPage();
-							if (swapPageNum == (uint32_t)-1) {
-								return swapPageNum;
-							}
-							m_writePage(pte[k].frameNumber, swapPageNum);
-							pte[k].present = 0;
-							assert(swapPageNum < m_SwapSize);
-							pte[k].frameNumber = swapPageNum;
-							pte[k].swaped = 1;
-							// Mark the page in bitMap according to goal
-							int i1 = rc / 4;
-							int i2 = (rc * 2) % 8;
-							switch (goal) {
-							case FOR_PAGE_DIR:
-								// level 1 page table. Mark as 01
-								m_BitMap[i1] |= (1 << i2);
-								break;
-							case FOR_PAGE_TABLE:
-								// Level 2 page table mark as 10
-								m_BitMap[i1] |= (1 << (i2 + 1));
-								break;
-							case FOR_PAGE:
-								// Address space page mark as 11
-								m_BitMap[i1] |= (3 << i2);
-								break;
-							}
-							if (rc >= m_MapSize) {
-								exit(5);
-							}
-							return rc;
-						}
-					}
-				}
-			}
-		}
-		return (uint32_t)-1;
-	}
-
-	/**
-	 * mark page as free
-	 *
-	 * \param[in] pageNum page number to be freed
-	 */
-	void freePage(uint32_t pageNum) {
-		uint8_t mask;
-		uint32_t index = pageNum / 4;
-		int shift = (pageNum * 2) % 8;
-		mask = 3 << shift;
-		if (m_BitMap[index] & mask) {
-			// Mark the bit as free
-			m_BitMap[index] &= ~mask;
-		} else {
-			cout << "error 2\n";
-			exit(1);
-		}
-	}
-
-	uint32_t nrPages() {
-		return m_MapSize;
-	}
-};
-CMemoryBitMap* g_Map;
-
-#endif // 0
+	cout << "\n";
+}
 
 FreeSpaceManager* g_FSMan;
 
@@ -533,10 +350,10 @@ void               memMgr                                  ( void            * m
 	
 	// Create instance of CCPU
 	uint32_t pTable = g_FSMan->allocatePage(true);
-	CMM mm((uint8_t*)mem, pTable * CCPU::PAGE_SIZE);
+	CMM* mm = new CMM((uint8_t*)mem, pTable * CCPU::PAGE_SIZE);
     
 	// Start init process
-	mainProcess(&mm, processArg);
-	
+	mainProcess(mm, processArg);
+	delete mm;
 	delete g_FSMan;
 }
